97ffa7e11f3c2d2bad540bc781a9b48f38a607d2
Uladzislau <yanushevskyv@gmail.com>
2024-10-15 12:50:41 +0200

    feat : added rn

--- src/commands.js
+++ src/commands.js
@@ -3,6 +3,7 @@ import { goUpperDirectory } from "./functions/goUpperDirectory.js";
 import { goDedicatedFolder } from "./functions/goDedicatedFolder.js";
 import { addFile } from "./functions/addFile.js";
 import { removeFile } from "./functions/removeFile.js";
+import { renameFile } from "./functions/renameFile.js";
 
 export const commands = {
   ls: async (args) => await getList(args.path),
@@ -10,4 +11,5 @@ export const commands = {
   cd: async (args) => await goDedicatedFolder(args),
   add: async (args) => await addFile(args),
   rm: async (args) => await removeFile(args),
+  rn: async (args) => await renameFile(args),
 };

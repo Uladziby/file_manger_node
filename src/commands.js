582afc11d1d113e47727c7d1d2d6e38f09fa4492
Uladzislau <yanushevskyv@gmail.com>
2024-10-15 12:12:49 +0200

    feat : added add

--- src/commands.js
+++ src/commands.js
@@ -1,9 +1,11 @@
 import { getList } from "./functions/getList.js";
 import { goUpperDirectory } from "./functions/goUpperDirectory.js";
 import { goDedicatedFolder } from "./functions/goDedicatedFolder.js";
+import { addFile } from "./functions/addFile.js";
 
 export const commands = {
   ls: async (args) => await getList(args.path),
   up: async (args) => await goUpperDirectory(args),
   cd: async (args) => await goDedicatedFolder(args),
+  add: async (args) => await addFile(args),
 };
---
+++ src/functions/addFile.js
@@ -0,0 +1,21 @@
+import { writeFile, access } from "fs/promises";
+import path from "node:path";
+
+export const addFile = async (args) => {
+  const filePath = path.resolve(args.path, args.args);
+  const erMess = "FS operation failed";
+
+  try {
+  } catch (err) {
+    console.error(erMess);
+  }
+
+  await access(args.path).then(() => {
+    console.dir("The file was created");
+
+    writeFile(filePath, "", { flag: "wx" });
+  }),
+    () => {
+      throw new Error(erMess);
+    };
+};
